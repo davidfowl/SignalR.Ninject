a5cd0f37b478e993942cb86409595bc7bf14023f
David Fowler <davidfowl@gmail.com>
2011-10-09 13:42:59 -0700

    Initial commit

---
+++ SignalR.Ninject/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalR.Ninject")]
+[assembly: AssemblyDescription("A ninject dependency resolver implementation for SignalR.")]
+[assembly: AssemblyCompany("David Fowler, Damian Edwards")]
+[assembly: AssemblyCopyright("Copyright © David Fowler and Damian Edwards 2011")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("0.3.3.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
