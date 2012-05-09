00b88f14c1eb746ad28a3e7779d9a7e44eed4df5
David Fowler <davidfowl@gmail.com>
2012-05-09 01:46:15 -0700

    Updated version

--- SignalR.Ninject/Properties/AssemblyInfo.cs
+++ SignalR.Ninject/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@
 
 [assembly: AssemblyVersion("0.5.0.0")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0-pre")]
+[assembly: AssemblyInformationalVersion("0.5.0.0")]
